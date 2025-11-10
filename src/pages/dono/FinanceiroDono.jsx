958fcd33238e9d91350285c603cd3ecd3bef648b
DiegoGenuino <genuinodiego11@gmail.com>
2025-11-10 16:28:16 -0300

    feat: create FinanceiroDono component for financial tracking

---
+++ src/pages/dono/FinanceiroDono.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FinanceiroDono = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Financeiro</h1>
+      <p>Acompanhe suas receitas e despesas.</p>
+    </div>
+  );
+};
+
+export default FinanceiroDono;
