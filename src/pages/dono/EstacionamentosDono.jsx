79c9f822f03724a64569b5951af2d0574ae8df4e
DiegoGenuino <genuinodiego11@gmail.com>
2025-11-10 16:28:11 -0300

    feat: create EstacionamentosDono component for managing parking lots

---
+++ src/pages/dono/EstacionamentosDono.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EstacionamentosDono = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Meus Estacionamentos</h1>
+      <p>Gerencie seus estacionamentos aqui.</p>
+    </div>
+  );
+};
+
+export default EstacionamentosDono;
