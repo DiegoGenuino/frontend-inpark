92a91b5b2e3310cdd735edef9c1263e95a8f88a2
DiegoGenuino <genuinodiego11@gmail.com>
2025-11-10 16:28:26 -0300

    feat: create PerfilDono component for managing user profile information

---
+++ src/pages/dono/PerfilDono.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PerfilDono = () => {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Meu Perfil</h1>
+      <p>Gerencie suas informações pessoais.</p>
+    </div>
+  );
+};
+
+export default PerfilDono;
