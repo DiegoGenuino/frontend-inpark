658db5fb192a89a9fc17b21d4e2657f823cc2253
DiegoGenuino <genuinodiego11@gmail.com>
2025-11-29 18:08:12 -0300

    feat: add Header and ReservasTable components to shared exports

--- src/components/shared/index.js
+++ src/components/shared/index.js
@@ -7,3 +7,5 @@ export { default as ParkingCard } from './ParkingCard';
 export { default as CarCard } from './CarCard';
 export { default as Toast } from './Toast';
 export { default as PageHeader } from './PageHeader';
+export { default as Header } from './Header';
+export { default as ReservasTable } from './ReservasTable';
